00c382fb57a96a9570b82adc62ffa773303b204e
aidanmao2005 <aidanmao2005@gmail.com>
2025-04-21 22:04:35 -0400

    homepage skeleton

--- composeApp/src/wasmJsMain/kotlin/org/xephyrous/components/DefaultSidebar.kt
+++ composeApp/src/wasmJsMain/kotlin/org/xephyrous/components/DefaultSidebar.kt
@@ -8,8 +8,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
 import org.jetbrains.compose.resources.painterResource
-import platformx.composeapp.generated.resources.Res
-import platformx.composeapp.generated.resources.Return
+import platformx.composeapp.generated.resources.*
 
 @Composable
 fun sidebar(
@@ -48,8 +47,112 @@ fun sidebar(
                 painter = painterResource(Res.drawable.Return), // replace with actual back logo
                 contentDescription = "Site Back Button"
             ) {
-                navController.navigate("Homepage")
+                navController.navigate("AnonymousHomepage")
             }
+        }
+    }
+}
+
+@Composable
+fun homeSidebar(
+    navController: NavController,
+    textSize: TextUnit = 14.sp,
+    alignment: OutlineBoxTitleAlignment = OutlineBoxTitleAlignment.OVERHANG,
+    alignmentSpacing: Dp = 30.dp
+) {
+    Box(
+        Modifier
+            .fillMaxHeight()
+            .width(90.dp)
+    ) {
+        Column (
+            Modifier.fillMaxSize().align(alignment = Alignment.Center)
+        ) {
+            Spacer(modifier = Modifier.height(60.dp))
+            clickableOutlineImage(
+                title = "About",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.About),
+                contentDescription = "About"
+            ) {
+                navController.navigate("About")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Profile",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.Profile),
+                contentDescription = "Profile"
+            ) {
+                navController.navigate("Profile")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Contact",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.Contact),
+                contentDescription = "Contact"
+            ) {
+                navController.navigate("Contact")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Calendar",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.About),
+                contentDescription = "Calendar"
+            ) {
+                navController.navigate("Calendar")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Schedule",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.Schedule),
+                contentDescription = "Schedule"
+            ) {
+                navController.navigate("Schedule")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Courses",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.Courses),
+                contentDescription = "Courses"
+            ) {
+                navController.navigate("Courses")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
+            clickableOutlineImage(
+                title = "Admin",
+                size = DpSize(70.dp, 70.dp),
+                textSize = textSize,
+                alignment = alignment,
+                alignmentSpacing = alignmentSpacing,
+                painter = painterResource(Res.drawable.Admin),
+                contentDescription = "Admin"
+            ) {
+                navController.navigate("Admin")
+            }
+            Spacer(modifier = Modifier.height(55.dp))
         }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/org/xephyrous/views/Homepage.kt
+++ composeApp/src/wasmJsMain/kotlin/org/xephyrous/views/Homepage.kt
@@ -9,25 +9,28 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
 import org.xephyrous.apis.OAuth
+import org.xephyrous.components.homepageTemplate
 import org.xephyrous.data.ViewModel
 
 @Composable
 fun Homepage(navController: NavController, viewModel: ViewModel, modifier: Modifier = Modifier) {
-    val coroutineScope = rememberCoroutineScope()
+    homepageTemplate(navController) {
+        val coroutineScope = rememberCoroutineScope()
 
-    Column {
-        Text("Homepage")
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    OAuth.redirect(
-                        scope = arrayOf("openid", "email", "https://www.googleapis.com/auth/datastore"),
-                        prompt = arrayOf("consent", "select_account")
-                    )
+        Column {
+            Text("Homepage")
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        OAuth.redirect(
+                            scope = arrayOf("openid", "email", "https://www.googleapis.com/auth/datastore"),
+                            prompt = arrayOf("consent", "select_account")
+                        )
+                    }
                 }
+            ) {
+                Text("Log In!")
             }
-        ) {
-            Text("Log In!")
         }
     }
 }
