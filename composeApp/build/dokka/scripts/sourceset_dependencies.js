520fa22c65c73715b43413d8a01c8b72a46cd451
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2025-04-30 15:09:19 -0400

    Correct build folder

---
+++ composeApp/build/dokka/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":composeApp:dokkaHtml/commonMain":[],":composeApp:dokkaHtml/wasmJsMain":[":composeApp:dokkaHtml/commonMain"]}'
---
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/platformx/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -0,0 +1,289 @@
+@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+
+package platformx.composeapp.generated.resources
+
+import kotlin.OptIn
+import kotlin.String
+import kotlin.collections.MutableMap
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.InternalResourceApi
+
+private object CommonMainDrawable0 {
+  public val About: DrawableResource by 
+      lazy { init_About() }
+
+  public val Admin: DrawableResource by 
+      lazy { init_Admin() }
+
+  public val Calendar: DrawableResource by 
+      lazy { init_Calendar() }
+
+  public val Contact: DrawableResource by 
+      lazy { init_Contact() }
+
+  public val Courses: DrawableResource by 
+      lazy { init_Courses() }
+
+  public val Event: DrawableResource by 
+      lazy { init_Event() }
+
+  public val Profile: DrawableResource by 
+      lazy { init_Profile() }
+
+  public val Return: DrawableResource by 
+      lazy { init_Return() }
+
+  public val Schedule: DrawableResource by 
+      lazy { init_Schedule() }
+
+  public val entire_network: DrawableResource by 
+      lazy { init_entire_network() }
+
+  public val icons: DrawableResource by 
+      lazy { init_icons() }
+
+  public val logo: DrawableResource by 
+      lazy { init_logo() }
+
+  public val modify_courses: DrawableResource by 
+      lazy { init_modify_courses() }
+
+  public val modify_data: DrawableResource by 
+      lazy { init_modify_data() }
+
+  public val modify_events: DrawableResource by 
+      lazy { init_modify_events() }
+
+  public val modify_users: DrawableResource by 
+      lazy { init_modify_users() }
+
+  public val no_view: DrawableResource by 
+      lazy { init_no_view() }
+
+  public val view: DrawableResource by 
+      lazy { init_view() }
+}
+
+@InternalResourceApi
+internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
+  map.put("About", CommonMainDrawable0.About)
+  map.put("Admin", CommonMainDrawable0.Admin)
+  map.put("Calendar", CommonMainDrawable0.Calendar)
+  map.put("Contact", CommonMainDrawable0.Contact)
+  map.put("Courses", CommonMainDrawable0.Courses)
+  map.put("Event", CommonMainDrawable0.Event)
+  map.put("Profile", CommonMainDrawable0.Profile)
+  map.put("Return", CommonMainDrawable0.Return)
+  map.put("Schedule", CommonMainDrawable0.Schedule)
+  map.put("entire_network", CommonMainDrawable0.entire_network)
+  map.put("icons", CommonMainDrawable0.icons)
+  map.put("logo", CommonMainDrawable0.logo)
+  map.put("modify_courses", CommonMainDrawable0.modify_courses)
+  map.put("modify_data", CommonMainDrawable0.modify_data)
+  map.put("modify_events", CommonMainDrawable0.modify_events)
+  map.put("modify_users", CommonMainDrawable0.modify_users)
+  map.put("no_view", CommonMainDrawable0.no_view)
+  map.put("view", CommonMainDrawable0.view)
+}
+
+internal val Res.drawable.About: DrawableResource
+  get() = CommonMainDrawable0.About
+
+private fun init_About(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:About",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/About.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Admin: DrawableResource
+  get() = CommonMainDrawable0.Admin
+
+private fun init_Admin(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Admin",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Admin.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Calendar: DrawableResource
+  get() = CommonMainDrawable0.Calendar
+
+private fun init_Calendar(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Calendar",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Calendar.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Contact: DrawableResource
+  get() = CommonMainDrawable0.Contact
+
+private fun init_Contact(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Contact",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Contact.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Courses: DrawableResource
+  get() = CommonMainDrawable0.Courses
+
+private fun init_Courses(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Courses",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Courses.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Event: DrawableResource
+  get() = CommonMainDrawable0.Event
+
+private fun init_Event(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Event",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Event.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Profile: DrawableResource
+  get() = CommonMainDrawable0.Profile
+
+private fun init_Profile(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Profile",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Profile.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Return: DrawableResource
+  get() = CommonMainDrawable0.Return
+
+private fun init_Return(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Return",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Return.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.Schedule: DrawableResource
+  get() = CommonMainDrawable0.Schedule
+
+private fun init_Schedule(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:Schedule",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/Schedule.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.entire_network: DrawableResource
+  get() = CommonMainDrawable0.entire_network
+
+private fun init_entire_network(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:entire_network",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/entire_network.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.icons: DrawableResource
+  get() = CommonMainDrawable0.icons
+
+private fun init_icons(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:icons",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/icons", -1, -1),
+    )
+)
+
+internal val Res.drawable.logo: DrawableResource
+  get() = CommonMainDrawable0.logo
+
+private fun init_logo(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:logo",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/logo.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.modify_courses: DrawableResource
+  get() = CommonMainDrawable0.modify_courses
+
+private fun init_modify_courses(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:modify_courses",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/modify_courses.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.modify_data: DrawableResource
+  get() = CommonMainDrawable0.modify_data
+
+private fun init_modify_data(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:modify_data",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/modify_data.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.modify_events: DrawableResource
+  get() = CommonMainDrawable0.modify_events
+
+private fun init_modify_events(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:modify_events",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/modify_events.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.modify_users: DrawableResource
+  get() = CommonMainDrawable0.modify_users
+
+private fun init_modify_users(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:modify_users",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/modify_users.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.no_view: DrawableResource
+  get() = CommonMainDrawable0.no_view
+
+private fun init_no_view(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:no_view",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/no_view.png", -1, -1),
+    )
+)
+
+internal val Res.drawable.view: DrawableResource
+  get() = CommonMainDrawable0.view
+
+private fun init_view(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:view",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/platformx.composeapp.generated.resources/drawable/view.png", -1, -1),
+    )
+)
